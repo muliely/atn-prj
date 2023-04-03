59e3f019549e1e922e50a74ddd0ff76426a91ae5
ThuyHoangThu <thuy.hoangthu@immutable.com>
2023-04-03 18:18:42 +0700

    heh

--- src/admin/config/connect.php
+++ src/admin/config/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$connect = mysqli_connect('localhost', 'root', '', 'assignment2_autoshop');
+	$connect = mysqli_connect('db', 'thuy', 'thuy@123', 'assignment2_autoshop');
 	if($connect) mysqli_query($connect, "set names 'utf8'");
 	else echo "DB ERROR";
 ?>
