c2d107fd5e1943ac943fb28d39efa241e2b1f056
Thuy Vu <thuy.hoangthu@immutable.com>
2023-04-11 15:41:54 +0700

    Add Database Connection

--- src/admin/config/connect.php
+++ src/admin/config/connect.php
@@ -6,11 +6,7 @@ $db_password = 'dbPass@123';
 $db_name = 'online';
 $db_host = 'db';
 
-$shipping_cost      = 1.50; //shipping cost
-$taxes              = array( //List your Taxes percent here.
-                            'VAT' => 12, 
-                            'Service Tax' => 5
-                            );						
+				
 //connect to MySql						
 $connect = new mysqli($db_host, $db_username, $db_password,$db_name);						
 if ($connect->connect_error) {
