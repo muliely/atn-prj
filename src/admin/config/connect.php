472563bbfc79e1610f17c8cc069d1ce2db9ede1f
Thuy Vu <thuy.hoangthu@immutable.com>
2023-04-11 14:16:05 +0700

    heh

--- src/admin/config/connect.php
+++ src/admin/config/connect.php
@@ -12,8 +12,8 @@ $taxes              = array( //List your Taxes percent here.
                             'Service Tax' => 5
                             );						
 //connect to MySql						
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);						
-if ($mysqli->connect_error) {
-    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+$connect = new mysqli($db_host, $db_username, $db_password,$db_name);						
+if ($connect->connect_error) {
+    die('Error : ('. $connect->connect_errno .') '. $connect->connect_error);
 }
 ?>
--- src/templates/header/menu.php
+++ src/templates/header/menu.php
@@ -2,14 +2,14 @@
     <div id="menu" class="collapse navbar-collapse">
         <ul>
         <?php
-    $data = mysqli_query($mysqli, "select * from category order by cat_id");
+    $data = mysqli_query($connect, "select * from category order by cat_id");
     if($data){
         while($row = mysqli_fetch_array($data)){
             echo '<li class="menu-item"><a href="index.php?page_layout=category&id='.$row['cat_id'].'">'.$row['cat_name'].'</a></li>';
         }
     }
     else{
-        echo "Error fetching data: " . mysqli_error($mysqli);
+        echo "Error fetching data: " . mysqli_error($connect);
     }
 ?>
         
