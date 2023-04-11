280886bf58a4f4283d204d3ebf2d619554b0161b
Thuy Vu <thuy.hoangthu@immutable.com>
2023-04-11 14:03:20 +0700

    heh

--- src/templates/header/menu.php
+++ src/templates/header/menu.php
@@ -1,19 +1,18 @@
 <nav>
     <div id="menu" class="collapse navbar-collapse">
         <ul>
-            <?php
-                $data = mysqli_query($connect, "select * from category order by cat_id");
-                while($row = mysqli_fetch_array($data)){
-                    echo '<li class="menu-item"><a href="index.php?page_layout=category&id='.$row['cat_id'].'">'.$row['cat_name'].'</a></li>';
-                    
-                }
-            ?>
-            <!-- <li class="menu-item"><a href="#">iPhone</a></li>
-            <li class="menu-item"><a href="#">Samsung</a></li>
-            <li class="menu-item"><a href="#">HTC</a></li>
-            <li class="menu-item"><a href="#">Nokia</a></li>
-            <li class="menu-item"><a href="#">Sony</a></li>
-            <li class="menu-item"><a href="#">Blackberry</a></li> -->
+        <?php
+    $data = mysqli_query($connect, "select * from category order by cat_id");
+    if($data){
+        while($row = mysqli_fetch_array($data)){
+            echo '<li class="menu-item"><a href="index.php?page_layout=category&id='.$row['cat_id'].'">'.$row['cat_name'].'</a></li>';
+        }
+    }
+    else{
+        echo "Error fetching data: " . mysqli_error($connect);
+    }
+?>
+        
         </ul>
     </div>
 </nav>
